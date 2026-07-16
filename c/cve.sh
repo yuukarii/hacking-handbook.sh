8f6c85a0b85a41b2af4e1596f945c58e2d61117f
yuukarii <48701900+yuukarii@users.noreply.github.com>
2026-07-16 23:13:19 +0700

    Add CVE analysis and Pacman rollback notes

---
+++ c/cve.sh
@@ -0,0 +1,24 @@
+# CVE analysis
+
+# Usage context:
+#   - Which component/package is vulnerable? affected versions / version ranges?
+#   - When, where, how is it used? direct or transitive dependency?
+#   - Which part of component is vulnerable? What kind of vulnerability?
+#   - List of CVEs, environmental CVSS scores and vectors
+#   - Overall assessment: HIGH, MEDIUM, LOW, Not Applicable
+
+# Probability:
+#   - Level of probability
+#   - Prerequisites/preconditions — auth required? specific config flag enabled? non-default build option?
+#   - The conditions to exploit, the exploit paths
+#   - How difficult of those paths? Public exploit availability?
+
+# Impacts:
+#   - Level of impact
+#   - Which aspects it impacts? Confidentiality? Integrity? Availability?
+#   - Does the scope change?
+#   - Which features, components, information would be impacted?
+#   - Which kind of sensitive data would be impacted?
+
+# Mitigation:
+#   - Which version of product will include the fixes?
--- p/pacman.sh
+++ p/pacman.sh
@@ -5,3 +5,6 @@ sudo pacman -U \
   /var/cache/pacman/pkg/bluez-utils-5.86-6-x86_64.pkg.tar.zst
 sudo systemctl restart bluetooth
 systemctl --user restart pipewire pipewire-pulse wireplumber
+
+vim /etc/pacman.conf
+# IgnorePkg = bluez*
